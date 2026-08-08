d01e5953d6f6441cbc651ae4e57ca6a2a29abd5a
evkescobar <evkescobar@gmail.com>
2026-08-07 20:32:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
     window.addEventListener('DOMContentLoaded', () => {
         const section = document.getElementById('delayed-section');
 
-        // 1. Show the section after 5 seconds
+        // 1. Show the section after 3 seconds
         setTimeout(() => {
             section.style.display = 'block';
-        }, 5000);
+        }, 3000);
 
         // 2. Hide the section after 10 seconds from page load
         setTimeout(() => {
